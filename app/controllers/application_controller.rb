89437ca9d8f457931690ef57974d6cdf6e507151
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-04-01 16:39:17 +0000

    implement style user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,23 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  private
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
 
+  def authorize
+    redirect_to '/login' unless current_user
+  end
+
+  private
   def cart
     @cart ||= cookies[:cart].present? ? JSON.parse(cookies[:cart]) : {}
   end
   helper_method :cart
 
   def enhanced_cart
-    @enhanced_cart ||= Product.where(id: cart.keys).map {|product| { product:product, quantity: cart[product.id.to_s] } }
+    @enhanced_cart ||= Product.where(id: cart.keys).map { |product| { product:product, quantity: cart[product.id.to_s] } }
   end
   helper_method :enhanced_cart
 
